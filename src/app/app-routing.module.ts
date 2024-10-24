5fbe51a65a3d619801d0c5a66908d068e9e14a1b
Ravindu Ponnamperuma <90395607+RavinduPonnamperuma@users.noreply.github.com>
2024-10-24 12:50:28 +0530

    create greenhoue ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  // {
+  //   path: 'login',
+  //   loadComponent: () => import('./core/index').then(com => com.LoginComponent),
+  //   // canActivate:[LoginGuard]
+  // },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
