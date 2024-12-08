ce450b1f84d32e23055dfc3a218b46f1ff0735cc
Ravindu Ponnamperuma <90395607+RavinduPonnamperuma@users.noreply.github.com>
2024-12-08 11:44:46 +0530

    create home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 import {NgIf} from "@angular/common";
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  showLoginForm: boolean = false;
+
+  constructor(private router: Router) {}
+
+
+  toggleRegisterForm(): void {
+    this.router.navigate(['/register']);
+  }
+
+}
