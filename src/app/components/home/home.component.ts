4d3d1fba271094612ade6f248937595d26c6b103
thimirasadeesha <thimira@maximimpressions.com>
2024-12-08 07:59:35 +0530

    register form added with validations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    ReactiveFormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registrationForm!: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      firstName: ['', [
+        Validators.required,
+        Validators.minLength(2)
+      ]],
+      lastName: ['', [
+        Validators.required,
+        Validators.minLength(2)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      phone: ['', [
+        Validators.required,
+        Validators.pattern(/^\d{3}-\d{2}-\d{3}$/)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]],
+      confirmPassword: ['', [
+        Validators.required
+      ]],
+      terms: [false, Validators.requiredTrue]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    return password && confirmPassword && password.value === confirmPassword.value
+      ? null
+      : { passwordMismatch: true };
+  }
+
+  get f() { return this.registrationForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // Stop here if form is invalid
+    if (this.registrationForm.invalid) {
+      return;
+    }
+
+    console.log(this.registrationForm.value);
+    alert('Registration Successful!');
+  }
+
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.registrationForm.get(fieldName);
+    return field ? (field.invalid && (field.dirty || field.touched || this.submitted)) : false;
+  }
 }
