c0d26fb8b78c60e05e99ef1b659fb64747d882b9
Ravindu Ponnamperuma <90395607+RavinduPonnamperuma@users.noreply.github.com>
2025-01-12 01:15:12 +0530

    Add sensor data to home page

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [
+
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
---
+++ src/app/services/mqtt-websocket.service.ts
@@ -0,0 +1,28 @@
+// src/app/services/mqtt-websocket.service.ts
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MqttWebSocketService {
+  private socket: Socket;
+  private readonly serverUrl = 'http://localhost:3000'; // Replace with your NestJS server URL
+
+  constructor() {
+    this.socket = io(this.serverUrl); // Connect to the WebSocket server
+  }
+
+  // Listen for sensor data from the server
+  listenToSensorData(): Observable<any> {
+    return new Observable((observer) => {
+      this.socket.on('sensorData', (data) => {
+        observer.next(data);
+      });
+
+      // Handle disconnection
+      return () => this.socket.disconnect();
+    });
+  }
+}
