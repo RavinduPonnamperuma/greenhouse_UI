6591fd15422f73f2abc7bf59b484c0320ad8cb61
Thimira Sadeesha <thimira@maximimpressions.com>
2025-06-07 22:36:29 +0530

    refactor: update environment configuration for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
-  production: false,
+  production: true,
 
   baseUrl : 'http://localhost:3001'
 };
+
+
+
+//
+// {
+//   "replace": "src/environments/environment.ts",
+//   "with": "src/environments/environment.development.ts"
+// }
