5973e97a1fb4bb5e261afd1f6ecfadd0382dee8b
Thimira Sadeesha <thimira@maximimpressions.com>
2025-08-02 08:51:36 +0530

    irrigation schedule added

---
+++ src/app/components/irrigation-schedule/irrigation-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-irrigation-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './irrigation-schedule.component.html',
+  styleUrl: './irrigation-schedule.component.scss'
+})
+export class IrrigationScheduleComponent {
+
+}
